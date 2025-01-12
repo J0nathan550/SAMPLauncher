2d5b5475dc6a062db1ca9c58bcb4ac09b99406ee
Ilya <88881607+J0nathan550@users.noreply.github.com>
2025-01-12 09:45:09 +0100

    Added a little bit of visuals

--- SAMPLauncher/Views/MainTabViews/FavoriteView.axaml.cs
+++ SAMPLauncher/Views/MainTabViews/FavoriteView.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace SAMPLauncher.Views.MainTabViews;
 
@@ -7,5 +8,10 @@ public partial class FavoriteView : UserControl
     public FavoriteView()
     {
         InitializeComponent();
+    }
+
+    private void FilterButtonToggle_Click(object? sender, RoutedEventArgs e)
+    {
+        FilterBorder.IsVisible = !FilterBorder.IsVisible;
     }
 }
